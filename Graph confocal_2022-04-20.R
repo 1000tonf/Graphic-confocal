f212570ad5210869c6b034bed053b0654925da25
yxu233 <tigerxu96@hotmail.com>
2022-05-02 15:53:36 -0400

    Adjust graph x database

--- Graph confocal_2022-04-20.R
+++ Graph confocal_2022-04-20.R
@@ -2,18 +2,19 @@ library(tidyverse)
 
 #large pc
 data_original=read.csv("G:/Confocal/D2 D4 O4 LC3 LAMP1 CQ DOC TRE N1 LG NG LAMP1 LC3-LAMP1 2022-04-19.csv", sep =",", header=TRUE)
-rename(data_original, Prep = ï..Prep ) -> data_original
+rename(data_original, Prep = ?..Prep ) -> data_original
+data_original=D2_D4_O4_LC3_LAMP1_CQ_DOC_TRE_N1_LG_NG_LAMP1_LC3_LAMP1_2022_04_29
 
 #Calculate vesicles per cell and summarize
 data_original %>%  
-  mutate(LAMP1_5_500_per_cell = LAMP1.5.500./Cell.., 
-    LAMP1_over500_per_cell = LAMP1..500./Cell.., 
-    LC3_5_500_per_cell = LC3.5.500./Cell.., 
-    LC3_over500_per_cell = LC3..500./Cell.., 
-    LAMP1.LC3.5.500_per_LAMP1 = LAMP1.LC3.5.500./LAMP1.5.500., 
-    LAMP1.LC3..500_per_LAMP1 = LAMP1.LC3..500./LAMP1..500.,
-    LAMP1.LC3.5.500_per_LC3 = LAMP1.LC3.5.500./LC3.5.500.,
-    LAMP1.LC3..500_per_LC3 = LAMP1.LC3..500./LC3..500.)%>% 
+  mutate(LAMP1_5_500_per_cell = `LAMP1(5-500)`/`Cell #`, 
+    LAMP1_over500_per_cell = `LAMP1(>500)`/`Cell #`, 
+    LC3_5_500_per_cell = `LC3(5-500)`/`Cell #`, 
+    LC3_over500_per_cell = `LC3(>500)`/`Cell #`, 
+    LAMP1.LC3.5.500_per_LAMP1 = `LAMP1+LC3(5-500)`/`LAMP1(5-500)`, 
+    LAMP1.LC3..500_per_LAMP1 = `LAMP1+LC3(>500)`/`LAMP1(>500)`,
+    LAMP1.LC3.5.500_per_LC3 = `LAMP1+LC3(5-500)`/`LC3(5-500)`,
+    LAMP1.LC3..500_per_LC3 = `LAMP1+LC3(>500)`/`LC3(>500)`)%>% 
   select (Prep, Day, Treatment, Condition, 
     LAMP1_5_500_per_cell, LAMP1_over500_per_cell, 
     LC3_5_500_per_cell, LC3_over500_per_cell, 
