893e68a36bba9992f30504969e2fdc0109e14a3d
Bioinformatics <mton_99@yahoo.com.br>
2022-06-20 16:09:49 -0400

    Small changes in the graphs

--- Before-after graph.R
+++ Before-after graph.R
@@ -39,22 +39,44 @@ theme_settings = theme(axis.line=element_line(size=1, colour="black"),panel.back
  axis.text=element_text(size=12, color="black", face=2), axis.title =element_text(size=14, color="black", face=2) , 
  strip.text = element_text(size=12, color="black", face=2))
 
-#significant bar coordinates
+##CTRL###################################
+
+#significant bar coordinates graph CTRL
 lines <-tibble(Condition_f = factor(c("LG", "NG"), levels = c("N1", "LG", "NG")),
-  x =c(1, 1), xend=c(2,2), y=c(20, 35), yend=y)
+  x =c(1, 1), xend=c(2,2), y=c(15, 35), yend=y)
 pvalues <- tibble(Condition_f = factor(c("LG", "NG"), levels = c("N1", "LG", "NG")), 
-  x =c(1.5, 1.5), y=c(22, 37), label = c("p = 0.085", "p = 0.009"))
+  x =c(1.5, 1.5), y=c(17, 37), label = c("p = 0.085", "p = 0.009"))
 
 #before-after graph CTRL
 data_CTRL %>% ggplot(aes(x=Day, y=LC3_per_cell_average, group=Prep))+
   facet_grid(~Condition_f)+ geom_line() + ylab("LC3 puncta/Cell")+ylim(0,40)+ geom_point()+theme_settings +
   geom_segment(data=lines, aes(x=x, y=y, xend=xend, yend=yend), inherit.aes = FALSE) +
   geom_text(data=pvalues, aes(x=x, y=y, label=label), inherit.aes = FALSE)
+
+
+
+#significant bar coordinates graph CTRL (LC3+LAMP1)
+lines_LC3.LAMP1 <-tibble(Condition_f = factor(c("NG"), levels = c("N1", "LG", "NG")),
+               x =c(1), xend=c(2), y=c(25), yend=y)
+pvalues_LC3.LAMP1 <- tibble(Condition_f = factor(c("NG"), levels = c("N1", "LG", "NG")), 
+                  x =c(1.5), y=c(27), label = c("p = 0.035"))
       
 #before-after graphs (LC3+LAMP1)
 data_CTRL %>% ggplot(aes(x=Day, y=LAMP1.LC3_per_cell_average, group=Prep))+
-  facet_grid(~Condition_f)+ geom_line() + ylab("LC3 + LAMP1 puncta/Cell")+ylim(0,40)+ geom_point()+theme_settings
+  facet_grid(~Condition_f)+ geom_line() + ylab("LC3 + LAMP1 puncta/Cell")+ylim(0,40)+ geom_point()+theme_settings+
+  geom_segment(data=lines_LC3.LAMP1, aes(x=x, y=y, xend=xend, yend=yend), inherit.aes = FALSE) +
+  geom_text(data=pvalues_LC3.LAMP1, aes(x=x, y=y, label=label), inherit.aes = FALSE)
+
+
+
+#significant bar coordinates graph CTRL NOT(LC3+LAMP1)
+lines_not_LC3.LAMP1 <-tibble(Condition_f = factor(c("NG"), levels = c("N1", "LG", "NG")),
+                         x =c(1), xend=c(2), y=c(17), yend=y)
+pvalues_not_LC3.LAMP1 <- tibble(Condition_f = factor(c("NG"), levels = c("N1", "LG", "NG")), 
+                            x =c(1.5), y=c(19), label = c("p = 0.001"))
 
 #before-after graphs NOT(LC3+LAMP1)
 data_CTRL %>% ggplot(aes(x=Day, y=LC3_not_LAMP1.LC3_per_cell_average, group=Prep))+
-  facet_grid(~Condition_f)+ geom_line() + ylab("LC3-only puncta/Cell")+ylim(0,40)+ geom_point()+theme_settings
+  facet_grid(~Condition_f)+ geom_line() + ylab("LC3 only puncta/Cell")+ylim(0,40)+ geom_point()+theme_settings+
+  geom_segment(data=lines_not_LC3.LAMP1, aes(x=x, y=y, xend=xend, yend=yend), inherit.aes = FALSE) +
+  geom_text(data=pvalues_not_LC3.LAMP1, aes(x=x, y=y, label=label), inherit.aes = FALSE)
