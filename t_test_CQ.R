aad059f2ad34fc09ec3456b256cefbd7213e82b4
Bioinformatics <mton_99@yahoo.com.br>
2022-07-06 16:17:03 -0400

    t test for CQ

---
+++ t_test_CQ.R
@@ -0,0 +1,76 @@
+#t-tests CQ D2xD4
+
+#LC3#################################################################################
+
+
+#CTRL N1 LC3
+data_CQ %>% select(Prep, Day, Treatment, Condition, LC3_per_cell_average) %>%
+  filter(Condition == "N1") %>% spread(Day, LC3_per_cell_average) -> data_CQ_N1_t_test
+
+boxplot(data_CQ_N1_t_test$D2, data_CQ_N1_t_test$D4)
+
+#Ho: Mean diff D2xD4 is 0
+# one-sided
+t.test(data_CQ_N1_t_test$D2, data_CQ_N1_t_test$D4, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.1642
+
+
+
+#CTRL LG LC3
+data_CQ %>% select(Prep, Day, Treatment, Condition, LC3_per_cell_average) %>%
+  filter(Condition == "LG") %>% spread(Day, LC3_per_cell_average) -> data_CQ_LG_t_test
+
+boxplot(data_CQ_LG_t_test$D2, data_CQ_LG_t_test$D4)
+
+#Ho: Mean diff D2xD4 is 0
+# one-sided
+t.test(data_CQ_LG_t_test$D2, data_CQ_LG_t_test$D4, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.07481
+
+
+
+#CTRL NG LC3
+data_CQ %>% select(Prep, Day, Treatment, Condition, LC3_per_cell_average) %>%
+  filter(Condition == "NG") %>% spread(Day, LC3_per_cell_average) -> data_CQ_NG_t_test
+
+boxplot(data_CQ_NG_t_test$D2, data_CQ_NG_t_test$D4)
+
+#Ho: Mean diff D2xD4 is 0
+# one-sided
+t.test(data_CQ_NG_t_test$D2, data_CQ_NG_t_test$D4, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.08672
+
+
+#LC3notLAMP1#################################################################################
+
+
+#CTRL N1 LC3notLAMP1
+data_CQ %>% select(Prep, Day, Treatment, Condition, LC3_not_LAMP1.LC3_per_cell_average) %>%
+  filter(Condition == "N1") %>% spread(Day, LC3_not_LAMP1.LC3_per_cell_average) -> data_CQ_N1_notLAMP1_t_test
+
+boxplot(data_CQ_N1_notLAMP1_t_test$D2, data_CQ_N1_notLAMP1_t_test$D4)
+
+#Ho: Mean diff D2xD4 is 0
+# one-sided
+t.test(data_CQ_N1_notLAMP1_t_test$D2, data_CQ_N1_notLAMP1_t_test$D4, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.1298
+
+
+
+#CTRL LG LC3notLAMP1
+data_CQ %>% select(Prep, Day, Treatment, Condition, LC3_not_LAMP1.LC3_per_cell_average) %>%
+  filter(Condition == "LG") %>% spread(Day, LC3_not_LAMP1.LC3_per_cell_average) -> data_CQ_LG_notLAMP1_t_test
+
+boxplot(data_CQ_LG_notLAMP1_t_test$D2, data_CQ_LG_notLAMP1_t_test$D4)
+
+#Ho: Mean diff D2xD4 is 0
+# one-sided
+t.test(data_CQ_LG_notLAMP1_t_test$D2, data_CQ_LG_notLAMP1_t_test$D4, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.1081
+
+
+
+#CTRL LG LC3notLAMP1
+data_CQ %>% select(Prep, Day, Treatment, Condition, LC3_not_LAMP1.LC3_per_cell_average) %>%
+  filter(Condition == "NG") %>% spread(Day, LC3_not_LAMP1.LC3_per_cell_average) -> data_CQ_NG_notLAMP1_t_test
+
+boxplot(data_CQ_NG_notLAMP1_t_test$D2, data_CQ_NG_notLAMP1_t_test$D4)
+
+#Ho: Mean diff D2xD4 is 0
+# one-sided
+t.test(data_CQ_NG_notLAMP1_t_test$D2, data_CQ_NG_notLAMP1_t_test$D4, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.0965
