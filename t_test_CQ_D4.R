70f9806a2083fc0aabd5946a395a91547e585120
Bioinformatics <mton_99@yahoo.com.br>
2022-07-07 10:41:23 -0400

    t-test CQ

--- t_test_CQ_D4.R
+++ t_test_CQ_D4.R
@@ -1,98 +1,60 @@
 #t-tests CQ D4
 
-#LAMP1(5-500)##########################################################################
+#LAMP1##########################################################################
 
-#CQ N1 LAMP1_5_500
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_5_500_per_cell_average) %>%
-  filter(Condition == "N1") %>% spread(Treatment, LAMP1_5_500_per_cell_average) -> data_CQ_t_test
-
-boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
-
-#Ho: Mean diff CTRLxCQ is 0
-# one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.8933
-
-
-
-#CQ LG LAMP1_5_500
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_5_500_per_cell_average) %>%
-  filter(Condition == "LG") %>% spread(Treatment, LAMP1_5_500_per_cell_average) -> data_CQ_t_test
+#CQ D4 N1 LAMP1
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_per_cell_average) %>%
+  filter(Condition == "N1") %>% spread(Treatment, LAMP1_per_cell_average) -> data_CQ_t_test
 
 boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
 
 #Ho: Mean diff CTRLxCQ is 0
 # one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.7354
+t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.8946
 
+###
 
-
-#CQ NG LAMP1_5_500
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_5_500_per_cell_average) %>%
-  filter(Condition == "NG") %>% spread(Treatment, LAMP1_5_500_per_cell_average) -> data_CQ_t_test
-
-boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
-
-#Ho: Mean diff CTRLxCQ is 0
-# one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.2307
-
-
-#LAMP1(>500)##########################################################################
-
-
-#CQ N1 LAMP1_>500
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_over500_per_cell_average) %>%
-  filter(Condition == "N1") %>% spread(Treatment, LAMP1_over500_per_cell_average) -> data_CQ_t_test
+#CQ D4 LG LAMP1
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_per_cell_average) %>%
+  filter(Condition == "LG") %>% spread(Treatment, LAMP1_per_cell_average) -> data_CQ_t_test
 
 boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
 
 #Ho: Mean diff CTRLxCQ is 0
 # one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.895
+t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.7344
 
+###
 
-
-#CQ LG LAMP1_>500
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_over500_per_cell_average) %>%
-  filter(Condition == "LG") %>% spread(Treatment, LAMP1_over500_per_cell_average) -> data_CQ_t_test
-
-boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
-
-#Ho: Mean diff CTRLxCQ is 0
-# one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.4486
-
-
-
-#CQ NG LAMP1_>500
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_over500_per_cell_average) %>%
-  filter(Condition == "NG") %>% spread(Treatment, LAMP1_over500_per_cell_average) -> data_CQ_t_test
+#CQ D4 NG LAMP1
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LAMP1_per_cell_average) %>%
+  filter(Condition == "NG") %>% spread(Treatment, LAMP1_per_cell_average) -> data_CQ_t_test
 
 boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
 
 #Ho: Mean diff CTRLxCQ is 0
 # one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.1997
+t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.2297
 
 
-#LC3(5-500)##########################################################################
+#LC3##########################################################################
 
 
-#CQ N1 LC3(5-500)
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_5_500_per_cell_average) %>%
-  filter(Condition == "N1") %>% spread(Treatment, LC3_5_500_per_cell_average) -> data_CQ_t_test
+#CQ D4 N1 LC3
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_per_cell_average) %>%
+  filter(Condition == "N1") %>% spread(Treatment, LC3_per_cell_average) -> data_CQ_t_test
 
 boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
 
 #Ho: Mean diff CTRLxCQ is 0
 # one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.1832
+t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.1837
 
+###
 
-
-#CQ LG LC3(5-500)
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_5_500_per_cell_average) %>%
-  filter(Condition == "LG") %>% spread(Treatment, LC3_5_500_per_cell_average) -> data_CQ_t_test
+#CQ D4 LG LC3
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_per_cell_average) %>%
+  filter(Condition == "LG") %>% spread(Treatment, LC3_per_cell_average) -> data_CQ_t_test
 
 boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
 
@@ -100,52 +62,52 @@ boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
 # one-sided
 t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.07493
 
+###
 
-
-#CQ NG LC3(5-500)
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_5_500_per_cell_average) %>%
-  filter(Condition == "NG") %>% spread(Treatment, LC3_5_500_per_cell_average) -> data_CQ_t_test
-
-boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
-
-#Ho: Mean diff CTRLxCQ is 0
-# one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.2489
-
-
-#LC3(>500)##########################################################################
-
-
-#CQ N1 LC3(>500)
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_over500_per_cell_average) %>%
-  filter(Condition == "N1") %>% spread(Treatment, LC3_over500_per_cell_average) -> data_CQ_t_test
+#CQ D4 NG LC3(5-500)
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_per_cell_average) %>%
+  filter(Condition == "NG") %>% spread(Treatment, LC3_per_cell_average) -> data_CQ_t_test
 
 boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
 
 #Ho: Mean diff CTRLxCQ is 0
 # one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.7228
+t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.2448
 
 
-
-#CQ LG LC3(>500)
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_over500_per_cell_average) %>%
-  filter(Condition == "LG") %>% spread(Treatment, LC3_over500_per_cell_average) -> data_CQ_t_test
-
-boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
-
-#Ho: Mean diff CTRLxCQ is 0
-# one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.7485
+#LC3notLAMP1#################################################################################
 
 
+#CQ D4 N1 LC3notLAMP1
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_not_LAMP1.LC3_per_cell_average) %>%
+  filter(Condition == "N1") %>% spread(Treatment, LC3_not_LAMP1.LC3_per_cell_average) -> data_CQ_N1_notLAMP1_t_test
 
-#CQ NG LC3(>500)
-data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_over500_per_cell_average) %>%
-  filter(Condition == "NG") %>% spread(Treatment, LC3_over500_per_cell_average) -> data_CQ_t_test
-
-boxplot(data_CQ_t_test$CTRL, data_CQ_t_test$CQ)
+boxplot(data_CQ_N1_notLAMP1_t_test$CTRL, data_CQ_N1_notLAMP1_t_test$CQ)
 
 #Ho: Mean diff CTRLxCQ is 0
 # one-sided
-t.test(data_CQ_t_test$CTRL, data_CQ_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.08149
+t.test(data_CQ_N1_notLAMP1_t_test$CTRL, data_CQ_N1_notLAMP1_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.1367
+
+###
+
+#CQ D4 LG LC3notLAMP1
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_not_LAMP1.LC3_per_cell_average) %>%
+  filter(Condition == "LG") %>% spread(Treatment, LC3_not_LAMP1.LC3_per_cell_average) -> data_CQ_LG_notLAMP1_t_test
+
+boxplot(data_CQ_LG_notLAMP1_t_test$CTRL, data_CQ_LG_notLAMP1_t_test$CQ)
+
+#Ho: Mean diff CTRLxCQ is 0
+# one-sided
+t.test(data_CQ_LG_notLAMP1_t_test$CTRL, data_CQ_LG_notLAMP1_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.1073
+
+###
+
+#CQ D4 NG LC3notLAMP1
+data_CQ_d4 %>% select(Prep, Treatment, Condition, LC3_not_LAMP1.LC3_per_cell_average) %>%
+  filter(Condition == "NG") %>% spread(Treatment, LC3_not_LAMP1.LC3_per_cell_average) -> data_CQ_NG_notLAMP1_t_test
+
+boxplot(data_CQ_NG_notLAMP1_t_test$CTRL, data_CQ_NG_notLAMP1_t_test$CQ)
+
+#Ho: Mean diff CTRLxCQ is 0
+# one-sided
+t.test(data_CQ_NG_notLAMP1_t_test$CTRL, data_CQ_NG_notLAMP1_t_test$CQ, mu=0, alt="less", paired=T) # with HA842, HA843, HA851 p-value=0.6265
